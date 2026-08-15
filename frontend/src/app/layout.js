559511ccf699b2b45f128d42a95c76d2e764c842
ankush0710 <ankushkurvey053@gmail.com>
2026-08-15 16:31:35 +0530

    feat: font family added

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,9 +1,20 @@
+import { Merriweather } from "next/font/google";
 import "./globals.css";
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  variable: "--font-merriweather",
+  weight: ["300", "400", "700", "900"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        className={`${merriweather.variable}  font-merriweather min-h-full flex flex-col`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
