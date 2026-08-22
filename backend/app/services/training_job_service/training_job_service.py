ac3377e0f5b5f9ad4e02ace219bf55a9040ab75a
Ankush Kurvey <ankushkurvey053@gmail.com>
2026-08-22 17:31:26 +0530

    training job service for create and rertieve training job has been created

---
+++ backend/app/services/training_job_service/training_job_service.py
@@ -0,0 +1,63 @@
+from sqlalchemy.orm import Session
+
+from app.model.training_model.training_model import Training_Model
+from app.model.training_job_model.training_job_model import TrainingJobModel
+from app.constants.training_status import training_status
+
+# ============================== To create the training jobs ============================ #
+def create_training_job(
+    db: Session,
+    training_run_id: int
+):
+
+    training_run = (
+        db.query(Training_Model)
+        .filter(
+            Training_Model.id == training_run_id
+        )
+        .first()
+    )
+
+    if not training_run:
+        return None
+
+    training_job = TrainingJobModel(
+        training_run_id=training_run_id,
+        status=training_status.QUEUED,
+        progress=0
+    )
+
+    db.add(training_job)
+
+    db.commit()
+
+    db.refresh(training_job)
+
+    return training_job
+
+# ============================== retrieve the trainng jobs ================================= #
+def get_training_jobs(
+    db: Session
+):
+
+    return (
+        db.query(TrainingJobModel)
+        .order_by(
+            TrainingJobModel.id.desc()
+        )
+        .all()
+    )
+
+# ============================= retrieve the training job by id ============================= #
+def get_training_job_by_id(
+    db: Session,
+    job_id: int
+):
+
+    return (
+        db.query(TrainingJobModel)
+        .filter(
+            TrainingJobModel.id == job_id
+        )
+        .first()
+    )
