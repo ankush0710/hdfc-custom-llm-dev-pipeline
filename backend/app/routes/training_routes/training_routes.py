b8a100fa3d7e66fbd604f71d0c9d763a515825d5
Ankush Kurvey <ankushkurvey053@gmail.com>
2026-08-22 15:01:32 +0530

    training  router has been created

--- backend/app/routes/training_routes/training_routes.py
+++ backend/app/routes/training_routes/training_routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.dbConfig.database_config import get_db
 from app.schema.training_schema.training_schema import (TrainingRunCreate, TrainingRunResponse)
-from app.services.training_service.training_service import create_training_run
+from app.services.training_service.training_service import (create_training_run, get_training_runs, get_training_run_by_id, start_training_run)
 
 router = APIRouter(
     prefix="/training",
@@ -22,3 +22,63 @@ def create_run(
     return create_training_run(db, data)
 
 
+@router.post(
+    "/runs/{run_id}/start",
+    response_model = TrainingRunResponse
+)
+def start_run(
+    run_id: int,
+    db: Session = Depends(get_db)
+):
+
+    try:
+        training_run = start_training_run(
+            db, run_id
+        )
+
+        if not training_run:
+            raise HTTPException(
+                status_code = 404,
+                detail = "training not found"
+            )
+
+        return training_run
+
+    except ValueError as error:
+        raise HTTPException(
+            status_code = 404,
+            detail = str(error)
+        )
+
+
+@router.get(
+    "/runs",
+    response_model = list[TrainingRunResponse]
+)
+def get_all_runs(
+    db: Session = Depends(get_db)
+):
+
+    return get_training_runs(db)
+
+
+@router.get(
+    "/runs/{run_id}",
+    response_model = TrainingRunResponse
+)
+def get_run_by_id(
+    run_id: int,
+    db:Session = Depends(get_db)
+):
+    training_run = get_training_run_by_id(
+        db,
+        run_id
+    )
+
+    if not training_run:
+        raise HTTPException(
+            status_code=404,
+            detail="Training run not found"
+        )
+
+    return training_run
