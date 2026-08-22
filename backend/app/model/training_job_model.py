94c0e0c3b5cd1bf7cf60f8be783bf655dc6575ad
Ankush Kurvey <ankushkurvey053@gmail.com>
2026-08-22 17:26:10 +0530

    training_job_ model has een created

---
+++ backend/app/model/training_job_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import (Column, String, Integer, DateTime, ForeignKey, Text)
+from sqlalchemy.sql import func
+from app.dbConfig.database_config import Base
+from app.constants.training_status import training_status
+
+class TrainingJobModel(Base):
+    __tablename__ = "training_job"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    training_run_id = Column(Integer, ForeignKey("training_run.id"), nullable=False)
+    status = Column(String, nullable=False, default=training_status.QUEUED)
+    worker_id = Column(String, nullable=False)
+    progress = Column(Integer, nullable=False, default=0)
+    error_message = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    started_at = Column(DateTime(timezone=True), nullable=True)
+    completed_at = Column(DateTime(timezone=True), nullable=True)
+    
+    
+    
+    
